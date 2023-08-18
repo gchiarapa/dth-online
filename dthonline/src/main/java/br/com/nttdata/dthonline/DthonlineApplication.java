8339fc5b02109ae9f7a7410ea411cada1d825ef7
gchiarapa <50337428+gchiarapa@users.noreply.github.com>
2023-08-18 14:39:18 -0300

    Add files via upload

---
+++ dthonline/src/main/java/br/com/nttdata/dthonline/DthonlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.nttdata.dthonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DthonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DthonlineApplication.class, args);
+	}
+
+}
